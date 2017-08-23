b968db58f8b06ccaa37a2ebd47dba630e1f92841
Fiore Basile <fiore.basile@gmail.com>
2017-08-23 23:16:28 +0200

    Update 2017-08-23T21:16:25.727Z

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const $ = gulpLoadPlugins();
 // Publishes the site to GitHub Pages
 gulp.task('publish', () => {  
   console.log('Publishing to GH Pages');
-  return gulp.src('./_book/**/*')
+  return gulp.src(['./CNAME', './_book/**/*'])
     .pipe($.ghPages({
       origin: 'origin',
       branch: 'gh-pages'
